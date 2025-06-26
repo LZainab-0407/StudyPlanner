a800c95f3d7b425996ca887c20c5dbfaa0b8955a
lzainab <143958152+lzainab@users.noreply.github.com>
2025-06-26 19:07:19 +0600

    Made changed to reduce code duplication

--- src/controllers/TaskController.java
+++ src/controllers/TaskController.java
@@ -28,72 +28,29 @@ import data.TaskManager;
 import models.PriorityLevel;
 import models.Task;
 import models.UserSession;
+import views.TaskFormPanel;
 import views.TaskListPanel;
 
 public class TaskController{
-
-	public static void openAddNewTaskWindow(JFrame parentFrame, JPanel mainContent) {
+	
+	/**
+	 * opens a new window with options and fields to create and add
+	 * a new task
+	 * @param parentFrame
+	 * @param mainContent
+	 */
+	public static void addNewTask(JFrame parentFrame, JPanel mainContent) {
 		
 		JFrame inputFrame = new JFrame("Add New Task");
 		inputFrame.setSize(500, 500);
 		inputFrame.setLocationRelativeTo(parentFrame);
 		inputFrame.setLayout(new BorderLayout());
 		
-		// panel for input
-		JPanel inputPanel = new JPanel(new GridBagLayout());
-		GridBagConstraints gbc = new GridBagConstraints();
-		
-		// first row, first column
-		JLabel titleLabel = new JLabel("Title: ");
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		gbc.insets = new Insets(5,5,5,5);
-		inputPanel.add(titleLabel, gbc);
-		
-		// first row, second column
-		JTextField titleField = new JTextField();
-		gbc.gridx = 1;
-		gbc.fill = GridBagConstraints.HORIZONTAL;
-		inputPanel.add(titleField, gbc);
-		
-		// second row, first column
-		JLabel descriptionLabel = new JLabel("Description: ");
-		gbc.gridx = 0;
-		gbc.gridy = 1;
-		gbc.fill = GridBagConstraints.HORIZONTAL;
-		inputPanel.add(descriptionLabel, gbc);
-		
-		// second row, second column
-		JTextArea descriptionArea = new JTextArea(4, 10);
-		JScrollPane scrollPane = new JScrollPane(descriptionArea);
-		gbc.gridx = 1;
-		gbc.fill = GridBagConstraints.HORIZONTAL;
-		inputPanel.add(scrollPane, gbc);
-		
-		// third row, first column
-		JLabel priorityLevelLabel = new JLabel("Priority Level: ");
-		gbc.gridx = 0;
-		gbc.gridy = 2;
-		gbc.fill = GridBagConstraints.NONE;
-		inputPanel.add(priorityLevelLabel, gbc);
-		
-		// third row, second column
-		JComboBox<PriorityLevel> priorityLevelBox = new JComboBox<>(PriorityLevel.values());
-		gbc.gridx = 1;
-		gbc.fill = GridBagConstraints.NONE;
-		inputPanel.add(priorityLevelBox, gbc);
-		
-		// fourth row, first column
-		JLabel deadlineLabel = new JLabel("Deadline: ");
-		gbc.gridx = 0;
-		gbc.gridy = 3;
-		inputPanel.add(deadlineLabel, gbc);
-		
-		// fourth row, second column
-		JDateChooser deadlinePicker = new JDateChooser();
-		deadlinePicker.setDateFormatString("dd MMMMM yyyy");
-		gbc.gridx = 1;
-		inputPanel.add(deadlinePicker, gbc);
+		TaskFormPanel inputPanel = new TaskFormPanel();
+		inputPanel.setTitleFieldText("");
+		inputPanel.setDescriptionText("");
+		inputPanel.setPriorityLevelBoxOption(PriorityLevel.HIGH);
+		inputPanel.setDeadlinePickerdate(new Date());
 		
 		inputFrame.add(inputPanel, BorderLayout.CENTER);
 		inputFrame.setVisible(true);
@@ -112,21 +69,17 @@ public class TaskController{
 		
 		// save button logic
 		saveButton.addActionListener(e -> {
-			String taskTitle = titleField.getText();
-			String taskDescription = descriptionArea.getText();
-			PriorityLevel taskPriorityLevel = (PriorityLevel) priorityLevelBox.getSelectedItem();
-			
-			Date selectedDate = deadlinePicker.getDate();
-			if (selectedDate == null) {
-				JOptionPane.showMessageDialog(inputFrame, "Please select a deadline date.");
-				return; // returns from actionPerformed method and newTask is not created
+			String taskTitle = inputPanel.getTitleText();
+			String taskDescription = inputPanel.getDescriptionText();
+			PriorityLevel taskPriorityLevel = inputPanel.getPriorityLevelFromBox();
+			if(inputPanel.getDeadlineFromPicker() == null) {
+				JOptionPane.showMessageDialog(inputPanel, "Please select a deadline");
+				return; // return from action performed method
 			}
-			// convert to LocalDate type referring to local zone
-			LocalDate deadlineDate = selectedDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-			// final deadline will be that day at 2359 hrs
-			LocalDateTime taskDeadline = LocalDateTime.of(deadlineDate, LocalTime.of(23, 59));
+			LocalDateTime taskDeadline = inputPanel.getDeadlineFromPicker();
 			
 			Task newTask = new Task(taskTitle, taskDescription, taskDeadline, taskPriorityLevel);
+			
 			// update task list
 			TaskManager.addTask(newTask);
 			// save updated task list to file
@@ -146,8 +99,10 @@ public class TaskController{
 		mainContent.removeAll();
 		JLabel pendingTasksLabel = new JLabel("Pending Tasks", SwingConstants.CENTER);
 		mainContent.add(pendingTasksLabel, BorderLayout.NORTH);
+		
 		TaskListPanel taskListPanel = new TaskListPanel(mainContent);
-		mainContent.add(taskListPanel, BorderLayout.CENTER);
+		JScrollPane taskListPane = new JScrollPane(taskListPanel);
+		mainContent.add(taskListPane, BorderLayout.CENTER);
 		mainContent.revalidate();
 	    mainContent.repaint();
 	}
@@ -170,84 +125,32 @@ public class TaskController{
 		changeFrame.setLayout(new BorderLayout());
 		changeFrame.setSize(500, 500);
 		
-		JPanel editPanel = new JPanel(new GridBagLayout());
-		GridBagConstraints gbc = new GridBagConstraints();
-		
-		// first row, first column
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		gbc.insets = new Insets(5,5,5,5);
-		
-		editPanel.add(infoPanel, gbc);
-		
-		// second row, first column
-		gbc.gridy = 1;
-		JLabel newTitleLabel = new JLabel("New Title: ");
-		editPanel.add(newTitleLabel, gbc);
-		
-		// third row, first column
-		gbc.gridy = 2;
-		JLabel newDescriptionLabel = new JLabel("New Description: ");
-		editPanel.add(newDescriptionLabel, gbc);
-		
-		// fourth row, first column
-		gbc.gridy = 3;
-		JLabel newPriorityLevelLabel = new JLabel("New Priority Level: ");
-		editPanel.add(newPriorityLevelLabel, gbc);
-		
-		// fifth row, first column
-		gbc.gridy = 4;
-		JLabel newDeadlineLabel = new JLabel("New Deadline: ");
-		editPanel.add(newDeadlineLabel, gbc);
-		
-		// second row, second column
-		JTextField titleField = new JTextField();
-		titleField.setText(task.getTitle());
-		gbc.gridx = 1;
-		gbc.gridy = 1;
-		gbc.fill = GridBagConstraints.HORIZONTAL;
-		editPanel.add(titleField, gbc);
-		
-		// third row, second column
-		JTextArea descriptionArea = new JTextArea(4, 10);
-		descriptionArea.setText(task.getDescription());
-		JScrollPane scrollPane = new JScrollPane(descriptionArea);
-		gbc.gridy = 2;
-		editPanel.add(scrollPane, gbc);
-		
-		// fourth row, second column
-		JComboBox<PriorityLevel> priorityLevelBox = new JComboBox<>(PriorityLevel.values());
-		gbc.gridy = 3;
-		gbc.fill = GridBagConstraints.NONE;
-		editPanel.add(priorityLevelBox, gbc);
-		
-		// fifth row, second column
-		JDateChooser deadlinePicker = new JDateChooser();
-		deadlinePicker.setDateFormatString("dd MMMMM yyyy");
-		gbc.gridy = 4;
-		editPanel.add(deadlinePicker, gbc);
-		
+		TaskFormPanel inputPanel = new TaskFormPanel();
+		inputPanel.setTitleFieldText(task.getTitle());
+		inputPanel.setDescriptionText(task.getDescription());
+		inputPanel.setPriorityLevelBoxOption(task.getPriorityLevel());
+		inputPanel.setDeadlinePickerdate(task.getDeadline());
 		
 		JPanel buttonPanel = new JPanel(new FlowLayout());
 		
 		JButton saveButton = new JButton("Save Changes");
 		saveButton.setFocusable(false);
 		buttonPanel.add(saveButton);
+		
 		saveButton.addActionListener(e -> {
-			task.setTitle(titleField.getText());
-			task.setDescription(descriptionArea.getText());
-			task.setPriorityLevel((PriorityLevel) priorityLevelBox.getSelectedItem());
-			if (deadlinePicker.getDate() != null) {
-				Date selectedDate = deadlinePicker.getDate();
-				LocalDate deadlineDate = selectedDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-				LocalDateTime taskDeadline = LocalDateTime.of(deadlineDate, LocalTime.of(23, 59));
-				task.setDeadline(taskDeadline);
+			task.setTitle(inputPanel.getTitleText());
+			task.setDescription(inputPanel.getDescriptionText());
+			task.setPriorityLevel(inputPanel.getPriorityLevelFromBox());
+			if(inputPanel.getDeadlineFromPicker() == null) {
+				JOptionPane.showMessageDialog(inputPanel, "Please select a deadline");
+				return; // return from action performed method
 			}
+			task.setDeadline(inputPanel.getDeadlineFromPicker());
+			
 			changeFrame.dispose();
 			TaskManager.saveTasksForUser(UserSession.getCurrentUser().getUsername());
 			TaskController.refresh(mainContent);
 		});
-		
 		
 		JButton cancelButton = new JButton("Cancel");
 		cancelButton.setFocusable(false);
@@ -255,24 +158,10 @@ public class TaskController{
 		cancelButton.addActionListener(e -> {changeFrame.dispose();});
 		buttonPanel.add(cancelButton);
 		
-		changeFrame.add(editPanel, BorderLayout.CENTER);
+		changeFrame.add(infoPanel, BorderLayout.NORTH);
+		changeFrame.add(inputPanel, BorderLayout.CENTER);
 		changeFrame.add(buttonPanel, BorderLayout.SOUTH);
 		changeFrame.setVisible(true);
-	}
-	
-	public static JPanel generateTaskInfoPanel(Task task) {
-		JPanel taskInfoPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		
-		JTextArea taskArea = new JTextArea(task.toString());
-		taskArea.setEditable(false);
-		taskArea.setOpaque(true);
-		
-		JScrollPane taskInfoPane = new JScrollPane(taskArea);
-		taskInfoPane.setFocusable(false);
-		
-		taskInfoPanel.add(taskInfoPane);
-		TaskListPanel.setPriorityColor(task, taskArea);
-		return taskInfoPanel;
 	}
 	
 }
---
+++ src/views/TaskFormPanel.java
@@ -0,0 +1,170 @@
+package views;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import com.toedter.calendar.JDateChooser;
+
+import models.PriorityLevel;
+
+public class TaskFormPanel extends JPanel {
+	
+	private JTextField titleField;
+	
+	private JTextArea descriptionArea;
+	
+	private JComboBox<PriorityLevel> priorityLevelBox;
+	
+	private JDateChooser deadlinePicker;
+	
+	public TaskFormPanel(){
+		this.setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		
+		// first row, first column
+		JLabel titleLabel = new JLabel("Title: ");
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.insets = new Insets(5,5,5,5);
+		this.add(titleLabel, gbc);
+		
+		// first row, second column
+		titleField = new JTextField();
+		gbc.gridx = 1;
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		this.add(titleField, gbc);
+		
+		// second row, first column
+		JLabel descriptionLabel =  new JLabel("Description: ");
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		this.add(descriptionLabel, gbc);
+		
+		// second row, second column
+		descriptionArea = new JTextArea(4, 10);
+		JScrollPane scrollPane = new JScrollPane(descriptionArea);
+		gbc.gridx = 1;
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		this.add(scrollPane, gbc);
+		
+		// third row, first column
+		JLabel priorityLevelLabel = new JLabel("Priority Level: ");
+		gbc.gridx = 0;
+		gbc.gridy = 2;
+		gbc.fill = GridBagConstraints.NONE;
+		this.add(priorityLevelLabel, gbc);
+		
+		// third row, second column
+		priorityLevelBox = new JComboBox<>(PriorityLevel.values());
+		gbc.gridx = 1;
+		gbc.fill = GridBagConstraints.NONE;
+		this.add(priorityLevelBox, gbc);
+		
+		// fourth row, first column
+		JLabel deadlineLabel =  new JLabel("Deadline: ");
+		gbc.gridx = 0;
+		gbc.gridy = 3;
+		this.add(deadlineLabel, gbc);
+		
+		// fourth row, second column
+		deadlinePicker = new JDateChooser();
+		deadlinePicker.setDateFormatString("dd MMMMM yyyy");
+		gbc.gridx = 1;
+		this.add(deadlinePicker, gbc);
+	}
+	
+	/**
+	 * returns text from the title field
+	 * @return
+	 */
+	public String getTitleText() {
+		return titleField.getText();
+	}
+	/**
+	 * sets initial text in the title field
+	 * @param title
+	 */
+	public void setTitleFieldText(String title) {
+		titleField.setText(title);
+	}
+	
+	/**
+	 * gets texyt from description area
+	 * @return
+	 */
+	public String getDescriptionText() {
+		return descriptionArea.getText();
+	}
+	
+	/**
+	 * sets initial text in description area
+	 * @param descriptionArea
+	 */
+	public void setDescriptionText(String description) {
+		descriptionArea.setText(description);;
+	}
+	
+	/**
+	 * returns priority level selected for the task
+	 * @return
+	 */
+	public PriorityLevel getPriorityLevelFromBox() {
+		return (PriorityLevel) priorityLevelBox.getSelectedItem();
+	}
+	
+	/**
+	 * Sets the initial priority level option for the box
+	 * @param priorityLevelBox
+	 */
+	public void setPriorityLevelBoxOption(PriorityLevel priorityLevel) {
+		priorityLevelBox.setSelectedItem(priorityLevel);;
+	}
+	
+	/**
+	 * returns deadline selected from deadline picker
+	 * @return
+	 */
+	public LocalDateTime getDeadlineFromPicker() {
+		Date selectedDate = deadlinePicker.getDate();
+		if (selectedDate == null) {
+			return null;
+		}
+		LocalDate date = selectedDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+		return LocalDateTime.of(date, LocalTime.of(23, 59));
+	}
+	
+	/**
+	 * Sets initial date on deadline picker 
+	 * @param deadline  (LocalDateTime) taskDeadline
+	 */
+	public void setDeadlinePickerdate(LocalDateTime deadline) {
+		if(deadline != null) {
+			 Date date = Date.from(deadline.atZone(ZoneId.systemDefault()).toInstant());
+	         deadlinePicker.setDate(date);
+		}
+	}
+	
+	/**
+	 * Sets initial date on deadline picker 
+	 * @param deadline  (Date) initial date
+	 */
+	public void  setDeadlinePickerdate(Date date) {
+		if (date != null) {
+			deadlinePicker.setDate(date);
+		}
+	}
+}
