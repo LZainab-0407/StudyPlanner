531608e7b6cd6ec52675fe463e1a5bb6c654eae3
lzainab <143958152+lzainab@users.noreply.github.com>
2025-06-26 16:37:39 +0600

    Updated multiple user functionality and edit button

--- src/views/MainFrame.java
+++ src/views/MainFrame.java
@@ -1,6 +1,7 @@
 package views;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -9,9 +10,13 @@ public class MainFrame extends JFrame {
 	
 	public MainFrame() {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(500, 500);
+		this.setSize(1000, 800);
 		this.setLayout(new BorderLayout());
 		this.setLocationRelativeTo(null);
+		
+		JPanel leftPanel = new JPanel();
+		leftPanel.setPreferredSize(new Dimension(150, 500));
+		this.add(leftPanel, BorderLayout.WEST);
 		
 		JPanel mainContent = new JPanel();
 		mainContent.setLayout(new BorderLayout());
@@ -20,7 +25,7 @@ public class MainFrame extends JFrame {
 		MyMenuBar myMenuBar = new MyMenuBar(this, mainContent);
 		this.setJMenuBar(myMenuBar);
 		
-		TaskManagerPanel taskManagerPanel = new TaskManagerPanel(this);
+		TaskManagerPanel taskManagerPanel = new TaskManagerPanel(this, mainContent);
 		this.add(taskManagerPanel, BorderLayout.EAST);
 		
 		this.setVisible(true);
